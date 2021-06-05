67354a155cd247e29df838b4da4cf0a40e729d90
Nitesh Ramola <niteshramola90@gmail.com>
2021-06-05 09:49:00 +0530

    added project screenshots

--- real_estate/settings.py
+++ real_estate/settings.py
@@ -96,7 +96,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = os.environ['EMAIL']
-EMAIL_HOST_PASSWORD = os.environ['REAL_ESTATE_PASSWORD']
+EMAIL_HOST_PASSWORD = os.environ['PASSWORD']
 EMAIL_USE_TLS =True
 
 
